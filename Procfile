1634d384cb3d5ad6d02bff0e2d0385a1dfe93d72
PedroAz <pdegutty@gmail.com>
2023-02-02 14:51:04 -0300

    deploying

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn projeto_cliente.wsgi
+web: gunicorn projeto_cliente.wsgi --log-file -
--- projeto_cliente/settings.py
+++ projeto_cliente/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -115,6 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 
 REST_FRAMEWORK = {
